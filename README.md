5f610bcabbd87b7a45f8abde8fdf568a08d3e471
wangrui <wangrui@hupu.com>
2019-08-29 19:39:46 +0800

    feat: ssr显示样式

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # webpack-demos-jk
 webpack深究demos
+
+### SSR
+> npm run build:ssr
+> node server/index.js
--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ if ( typeof window === 'undefined') {
     global.window = {};
 }
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { renderToString } = require('react-dom/server');
 const SSR = require('../dist/search-server.js');
+const template = fs.readFileSync(path.join(__dirname,'../dist/search.html'), 'utf-8')
+const data = require('./data.json');
 
 const server = (port) => {
     const app = express();
@@ -21,18 +25,7 @@ const server = (port) => {
 server(process.env.PORT || 3000);
 
 const renderMarkup = (str) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-        <div id="root">${str}</div>
-    </body>
-    </html>
-    `;
+    const dataSrc = JSON.stringify(data);
+    return template.replace('<!--HTML_PLACEHOLDER-->', str)
+        .replace('<!--INITIAL_DATA_PLACEHOLDER-->', `<script>window.__initial_data = ${dataSrc}</script>`);
 }
