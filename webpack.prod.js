077d649efad10954340fbc043507cb06c6442b85
wangrui <wangrui@hupu.com>
2019-09-04 21:16:03 +0800

    feat: 使用webpack对图片压缩

--- webpack.prod.js
+++ webpack.prod.js
@@ -126,7 +126,31 @@ module.exports = smp.wrap({
                         options: {
                             limit: 10240
                         }
-                    }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          mozjpeg: {
+                            progressive: true,
+                            quality: 65
+                          },
+                          // optipng.enabled: false will disable optipng
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                          },
+                          gifsicle: {
+                            interlaced: false,
+                          },
+                          // the webp option will enable WEBP
+                          webp: {
+                            quality: 75
+                          }
+                        }
+                    },
                 ]
             },
             {
